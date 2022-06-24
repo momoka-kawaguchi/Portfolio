12b04f795da667c22dec5a6bb8bf9278b9a5d38d
momoka kawaguchi <momojazz@gmail.com>
2022-06-24 10:13:05 +0900

    ボタンを追加

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -4,27 +4,53 @@
       <Title
         :class="title"
         :title="content.metadata.title"
-        description="Education and Learning"
+        description="Career and Learning"
       >
       </Title>
     </AnimateOnVisible>
 
-    <AnimateOnVisible name="fadeUp" :duration="1">
-      <div class="container-fluid">
-        <div class="row">
-          <ExperienceColumn
-            :posts="educations"
-            title="Education"
-            class="col-12 col-md left"
-          />
-          <ExperienceColumn
-            :posts="learning"
-            title="Learning"
-            class="col-12 col-md right"
-          />
+    <template v-if="isKiraKiraHistory">
+      <AnimateOnVisible name="fadeUp" :duration="1">
+        <div class="container-fluid">
+          <button @click="isKiraKiraHistory = !isKiraKiraHistory">
+            💔Real💔
+          </button>
+          <div class="row">
+            <ExperienceColumn
+              v-bind:posts="career"
+              title="Career"
+              class="col-12 col-md left"
+            />
+            <ExperienceColumn
+              :posts="learning"
+              title="Learning"
+              class="col-12 col-md right"
+            />
+          </div>
         </div>
-      </div>
-    </AnimateOnVisible>
+      </AnimateOnVisible>
+    </template>
+    <template v-if="!isKiraKiraHistory">
+      <AnimateOnVisible name="fadeUp" :duration="1">
+        <div class="container-fluid">
+          <button @click="isKiraKiraHistory = !isKiraKiraHistory">
+            🌟Virtual🌟
+          </button>
+          <div class="row">
+            <ExperienceColumn
+              v-bind:posts="careerTrue"
+              title="Career"
+              class="col-12 col-md left"
+            />
+            <ExperienceColumn
+              :posts="learningTrue"
+              title="Learning"
+              class="col-12 col-md right"
+            />
+          </div>
+        </div>
+      </AnimateOnVisible>
+    </template>
   </section>
 </template>
 
@@ -36,7 +62,8 @@ export default {
   name: "Experience",
   props: ["content"],
   data: () => ({
-    educations: [
+    isKiraKiraHistory: true,
+    career: [
       {
         year: "March 2018",
         title: "高校卒業",
@@ -46,23 +73,20 @@ export default {
       {
         year: "April 2018",
         title: "大学入学",
-        content:
-          "理工学部数学科に進学。純粋数学と情報数学を学ぶ。",
+        content: "理工学部数学科に進学。純粋数学と情報数学を学ぶ。",
       },
       {
         year: "March 2022",
         title: "大学卒業 ＆ 教員免許取得",
-        content:
-          "大学卒業と同時に、数学と情報の中高教員免許を取得。",
+        content: "大学卒業と同時に、数学と情報の中高教員免許を取得。",
       },
       {
         year: "April 2022",
         title: "大学院入学",
-        content:
-          "理工学研究科数学専攻に進学。代数学を研究。",
+        content: "理工学研究科数学専攻に進学。代数学を研究。",
       },
     ],
-     learning: [
+    learning: [
       {
         year: "April 2017",
         title: "プログラミングとの出会い",
@@ -75,19 +99,74 @@ export default {
         content:
           "大学では、C言語、JavaScript、Pythonを用いた授業を受講。\n主に情報技術におけるアルゴリズムについて学習する。",
       },
-       {
+      {
         year: "April 2019",
         title: "Latexでレポート提出",
         content:
-          '数学の講義レポートを提出する際に、マークアップ言語であるLaTexを用いて、資料を作成。\nまた、ICT教材であるGeoGebra、Wintpic、Mathematicaを用いて、動的教材を提案する。',
+          "数学の講義レポートを提出する際に、マークアップ言語であるLaTexを用いて、資料を作成。\nまた、ICT教材であるGeoGebra、Wintpic、Mathematicaを用いて、動的教材を提案する。",
       },
-       {
+      {
         year: "April 2022",
         title: "本格的な勉強！",
         content:
           "本格的にプログラミングを学びたいと思い、継続的に学習することを決意。\nProgateやUdemyを活用して、HTML、CSS、JavaScriptの学習を始める。\nまた、Vue.jsやGatsby(React)を用いて、ポートフォリオを作成。",
       },
     ],
+    careerTrue: [
+      {
+        year: "March 2018",
+        title: "高校卒業",
+        content:
+          "短期留学で、現在合コンに一緒に行く友達ができた❤️\n高校３年間で彼氏をつくって、青春をする予定だった…あれ…",
+      },
+      {
+        year: "April 2018",
+        title: "大学入学",
+        content:
+          "数学科の初彼氏をGET！3ヶ月で別れる）\nその後、元彼とゼミが同じになる…ゼミ生4人しかいないのに😇",
+      },
+      {
+        year: "March 2022",
+        title: "大学卒業 ＆ 教員免許取得",
+        content:
+          "悪い男の見極め方、女友達との付き合い方、酒の飲み方を学んだ（笑）\nそして、ビールの美味しさが分かるようになってしまった、、、",
+      },
+      {
+        year: "April 2022",
+        title: "大学院入学",
+        content:
+          "合コンデビュー👏合コンでは、”文系女子大学生”と自己紹介をする(^^)\n毎日ビールのむようになった（やめたい痩せたい）",
+      },
+    ],
+    learningTrue: [
+      {
+        year: "April 2017",
+        title: "プログラミングとの出会い",
+        content:
+          "「プログラミング」という言葉に憧れをもち、なんとなく興味をもつ😂",
+      },
+      {
+        year: "April 2018",
+        title: "大学での講義",
+        content:
+          "友達と助け合いながら必死に単位を取る🥺時には、ガストに深夜2時までいたことも（笑）\nとにかく助け合った（笑）そのおかげ❔で最高GPAは3.8（ ;  ; ）",
+      },
+      {
+        year: "April 2019",
+        title: "Latexでレポート提出",
+        content:
+          "「Latexで作成したレポートしか受け付けないわ！」という教授がいたから、しょうがなくLatexを、、\nコロナ禍でのレポート課題が多すぎて、気づいた頃にはWordよりもLatexで作成する方が早くなっていた😅",
+      },
+      {
+        year: "April 2022",
+        title: "本格的な勉強！",
+        content:
+          "知らないことを知っていくことが楽しい！！！！\nエラーを修復したときの快感すごい！！！！\nただ、エラーの原因がわからない時は、、、、、",
+      },
+    ],
+    return() {
+      this.isKiraKiraHistory = false;
+    },
   }),
   components: {
     Title,
