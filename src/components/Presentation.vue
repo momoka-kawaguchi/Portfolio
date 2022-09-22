01bc68ec2c8138c16ebd026be97f19f2b7acdc8b
momoka kawaguchi <momojazz36@gmail.com>
2022-09-22 11:35:28 +0900

    change: テキスト変更

--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -1,29 +1,30 @@
 <template>
   <div class="paragraph">
-    <h3>{{content.metadata.pres_title}}</h3>
-    <div class="begin">{{pres_first}}</div>
-    <p>{{pres_second}}</p>
+    <h3>{{ content.metadata.pres_title }}</h3>
+    <div class="begin">{{ pres_first }}</div>
+    <p>{{ pres_second }}</p>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'Description',
-    props: ['content'],
-data: () => ({
+  name: "Description",
+  props: ["content"],
+  data: () => ({
     pres_first: `エンジニアを目指す修士1年生。\n大学院では、数学を専攻。好きな公式は、オイラー積の公式！笑`,
-    pres_second: '大学の授業では、C,Python,JavaScriptを学習。\n現在は、Vue.jsを勉強中…(インターン探してます🥺)'
+    pres_second:
+      "大学の授業では、C,Python,JavaScriptを学習。\n現在は、Vue.js,TypeScriptを勉強中…",
   }),
-}
+};
 </script>
 
 <style scoped lang="scss">
-    @import '@/styles/constants.scss';
+@import "@/styles/constants.scss";
 
-    .paragraph {
-        color: map-get($colors, primary);
-        .begin {
-            color: map-get($colors, secondary);
-        }
-    }
+.paragraph {
+  color: map-get($colors, primary);
+  .begin {
+    color: map-get($colors, secondary);
+  }
+}
 </style>
